fd1b1269fcc1b5c65ef8932974ef7f8472d6404d
BasankoSV <basankosv@gmail.com>
2020-10-20 16:53:45 +0500

    delete slick carousel and add main carousel javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,13 @@
-$(document).ready(function(){
-  $('.carousel__inner').slick({
-    speed: 1200,
-    /* adaptiveHeight: true, */
-    prevArrow: `<button type="button" class="slick-prev">
-                  <img src="icons/left.svg">
-                </button>`,
-    nextArrow: `<button type="button" class="slick-next">
-                  <img src="icons/right.svg">
-                </button>`,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          arrows: false
-        }
-      }
-    ]
-  });
-});
+const imgItem = document.querySelector('.imgItem');
+const prev = document.querySelector('.arrow-prev');
+const next = document.querySelector('.arrow-next');
+
+const changeNameHundler = (sign, a, b) => {
+  currentImg = +imgItem.getAttribute('src').substr(10, 1);
+  nextImg =
+    currentImg + Number(`${sign}1`) == a ? b : currentImg + Number(`${sign}1`);
+  return (imgItem.src = `img/slide_${nextImg}.jpg`);
+};
+
+prev.addEventListener('click', () => changeNameHundler('-', 0, 3));
+next.addEventListener('click', () => changeNameHundler('+', 4, 1));
