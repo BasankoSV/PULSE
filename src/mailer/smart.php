8e6b6934ce863c04e728287bd27908d44238833f
BasankoSV <56435380+BasankoSV@users.noreply.github.com>
2020-11-12 21:06:16 +0500

    Update smart.php, email delete
    Delete email

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -14,13 +14,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'stas_nu';                 // Наш логин
-$mail->Password = '1234567890';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('stas_nu@mail.ru');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
